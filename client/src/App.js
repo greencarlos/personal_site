bf743dc7e642a83d296f8bce66d3eecda935aecc
Carlos Green <zonxa14@gmail.com>
2019-06-24 16:59:17 -0700

    react

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+	  Some cool react things
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
