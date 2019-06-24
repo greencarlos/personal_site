f725d8f9cd2a9124c60912ead04788a7116e8b69
Carlos Green <zonxa14@gmail.com>
2019-06-24 16:58:05 -0700

    Updates

---
+++ precache-manifest.2508360e7d6317aee4d8cf23df84eb68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d949ec7678f02ab510c603903b3697e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8583189358094bae9012",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "8583189358094bae9012",
+    "url": "/static/js/main.9b869bce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
