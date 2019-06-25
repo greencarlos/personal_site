9111adc77a42489ce5b860884d4b7a5096b28ed7
Carlos Green <zonxa14@gmail.com>
2019-06-24 19:31:09 -0700

    react

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,25 @@
-console.log("This is a cool console yoo!");
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'Thanks for like on my site!';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(LikeButton), domContainer);
