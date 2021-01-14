265a27c45d1d11d64a018d3e60afc828670e6f0d
Carlos Green <zonxa14@gmail.com>
2021-01-14 09:51:35 -0800

    configure css and basic js for chatbot functionality

---
+++ MachineLearning/JS/chat.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('.container');
+const submit = container.querySelector('.submit');
+const userMessage = container.querySelector('.userMessage');
+
+const train = container.querySelector('.train');
+const yesBtn = train.querySelector('.yesBtn');
+const noBtn = train.querySelector('.noBtn');
+
+const dialog = container.querySelector('.dialogBox')
+
+const debounce = (fn, time, timeout) => {
+	console.log('debounce called')
+	return () => {
+		const funcCall = () => fn()
+
+		clearTimeout(timeout)
+		timeout = setTimeout(funcCall, time)
+	}
+}
+
+const helpful = () => train.style.visibility = 'visible'
+const trainBot = () =>	train.style.visibility = 'hidden' 
+const wasHelpful = debounce(helpful, 15000)
+
+yesBtn.addEventListener('click', trainBot);
+noBtn.addEventListener('click', trainBot);
+
+userMessage.addEventListener('keyup', wasHelpful)
+
+submit.addEventListener('click', () => {
+  const message = userMessage.value;
+  dialog.innerHTML += `<p><b>${message}</b></p>`;
+
+  setTimeout(() => {
+    dialog.innerHTML += `
+		  <br/> <p><i>48</i></p>
+			`;
+  }, 2000);
+});
