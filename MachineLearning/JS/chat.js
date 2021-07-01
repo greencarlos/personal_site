dab6c640bbbd8448713698f9cd5b735929021d44
Carlos Green <zonxa14@gmail.com>
2021-06-30 19:38:21 -0700

    wiki api

--- MachineLearning/JS/chat.js
+++ MachineLearning/JS/chat.js
@@ -32,11 +32,25 @@ https://www.programmableweb.com/api/wikipedia-rest-api
 
 */
 
-const passage =
-  `Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet. The meaining of the universe is 42.Soon the robots will take over.Robots will be taking over soon.Robots will be self aware soon. This is all silly.It's is all fun and games until AI becomes a SuperIntelligence. 
+let passage =
+  `Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet. The meaining of the universe is 42. Soon the robots will take over. Robots will be taking over soon. Robots will be self aware soon. This is all silly. It's is all fun and games until AI becomes a SuperIntelligence. 
 	`;
 
+fetch('https://en.wikipedia.org/wiki/Universe', {mode: 'no-cors'})
+.then(res => console.log('res', res))
+//.then(res => res.json())
+
+/*
+fetch('https://en.wikipedia.org/wiki/Artificial_intelligence', {
+  credentials: 'same-origin',
+  Access-Control-Allow-Origin: 'https://en.wikipedia.org', 
+})
+.then(res => res.json())
+.then(res => console.log('res', res))
+*/
+
 submit.addEventListener('click', () => {
+
   const question = userQuestion.value.trim();
   dialog.innerHTML += `<p><b>${question}</b></p>`;
   console.log('passage', passage);
