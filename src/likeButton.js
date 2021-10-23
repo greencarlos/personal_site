fb056bd553a63173320fe2c36fb5642d49d92016
greenc <verdeclos@gmail.com>
2021-10-23 16:21:50 -0700

    update click react component

--- src/likeButton.js
+++ src/likeButton.js
@@ -1,25 +1,37 @@
-'use strict';
+"use strict";
 
 const e = React.createElement;
 
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = {
+      liked: false,
+      count: 0,
+    };
   }
 
   render() {
     if (this.state.liked) {
-      return 'Thank you!';
+      return e(
+        "p",
+        {
+          onClick: () => this.setState({ count: this.state.count + 1 }),
+        },
+        `Thank you! You've clicked: ${this.state.count} times`
+      );
     }
 
     return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like the site?'
+      "button",
+      {
+        onClick: () =>
+          this.setState({ liked: true, count: this.state.count + 1 }),
+      },
+      "Like the site?"
     );
   }
 }
 
-const domContainer = document.querySelector('#like');
+const domContainer = document.querySelector("#like");
 ReactDOM.render(e(LikeButton), domContainer);
